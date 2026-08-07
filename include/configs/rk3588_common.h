9e1e324d94b9cad8ec75207c14fb5177cb89f255
yifengyou <842056007@qq.com>
2026-08-07 09:19:13 +0800

    更新发布公告
    Signed-off-by: yifengyou <842056007@qq.com>

--- include/configs/rk3588_common.h
+++ include/configs/rk3588_common.h
@@ -137,9 +137,11 @@
 		"echo SCSI: no scsi bootable media; \0" \
 	"bootcmd_emmc=" \
 		"echo EMMC: scanning; " \
-		"setenv devtype mmc; " \
+		"setenv devtype mmc; "  \
+		"mmc rescan; mmc info; " \
 		"setenv devnum 0; if mmc dev 0; then run boot_one_dev; fi; " \
-		"echo MMC: no emmc bootable media; \0" \
+		"setenv devnum 1; if mmc dev 0; then run boot_one_dev; fi; " \
+		"echo EMMC: no emmc bootable media; \0" \
 	"bootcmd=run bootcmd_usb; run bootcmd_emmc; run bootcmd_nvme; run bootcmd_scsi; " \
 		"echo ERROR: No bootable device found; \0"
 
